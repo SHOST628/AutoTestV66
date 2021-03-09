9d99a2fe7026656ff4fa29fd617779d2615c7d08
Sincave <Sincave.Zhang@tech-trans.com>
2021-03-09 16:44:20 +0800

    add testcase doc: AutoTest user doc table demo testcase demo element location doc

--- runall/run.bat
+++ runall/run.bat
@@ -2,6 +2,6 @@
 set root=%~dp0
 set run_path=%root%run.py
 call %root%..\venv\Scripts\activate.bat
-python %root%run.py
+python %run_path%
 rem notice that you have to set  the value of VIRTUAL_ENV  on env/Scripts/activate.bat to %~dp0..  or the root path of env if you want to move env to other machine
-rem the best mehtod is set the value of VIRTUAL_ENV  on env/Scripts/activate.bat to %~dp0.. if  you want to move env to other machine
+rem the best mehtod is to set the value of VIRTUAL_ENV  on env/Scripts/activate.bat to %~dp0.. if  you want to move env to other machine
